65ebd1d8654e51ea8f519d2b4ef09c46e6a12c0a
nolleh7707 <nolleh7707@gmail.com>
2012-10-10 11:31:14 +0000

    add features for GCM

--- src/kr/a3cctv/client/MainActivity.java
+++ src/kr/a3cctv/client/MainActivity.java
@@ -3,9 +3,15 @@ package kr.a3cctv.client;
 import kr.a3cctv.client.camera.Preview;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
+import com.google.android.gcm.GCMRegistrar;
+
 public class MainActivity extends Activity {
+	
+	public static final String TAG = "hackfair";
+	public static final String SENDER_ID = "1000906162243";
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,8 @@ public class MainActivity extends Activity {
         Preview view = new Preview(this);
         
         setContentView(view);
+        
+        gcmRegister();
     }
 
     @Override
@@ -22,5 +30,16 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    
+    private void gcmRegister(){
+    	//에뮬레이터 등에서는 이용 불가. 체크
+        GCMRegistrar.checkDevice(this);
+        //permission 등.
+        GCMRegistrar.checkManifest(this);
+        final String regId = GCMRegistrar.getRegistrationId(this);
+        if (regId.equals("")) {
+        	GCMRegistrar.register(this, SENDER_ID);
+        } else {
+          Log.v(TAG, "Already registered");
+        }
+    }
 }
