89ed9d2cd56de39f9bd1f4aa7518b6397737cde5
ryukw82 <ryukw82@860ef7eb-1e7e-7184-4441-25ebcaecbddb>
2012-10-11 09:47:56 +0000

    아두이노쪽 수정 중 커밋

--- src/kr/a3cctv/client/ArduinoActivity.java
+++ src/kr/a3cctv/client/ArduinoActivity.java
@@ -28,8 +28,6 @@ public class ArduinoActivity extends Activity implements Runnable {
 	private static final String ACTION_USB_PERMISSION = "A3CCTV.USB_PERMISSION";
 	private static final int MESSAGE_ECHO = 1;
 
-	private static final byte COMMAND_LED = (byte) 1;
-
 	private boolean mPermissionRequestPending = false;
 	private UsbManager mUsbManager = null;
 	private UsbAccessory mAccessory;
@@ -40,15 +38,16 @@ public class ArduinoActivity extends Activity implements Runnable {
 	private PendingIntent mPermissionIntent;
 
 	private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
-		
+
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			String action = intent.getAction();
 			if (ACTION_USB_PERMISSION.equals(action)) {
 				synchronized (this) {
-//					UsbAccessory accessory = UsbManager.getAccessory(intent);
-					UsbAccessory accessory = (UsbAccessory) intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
-					
+					// UsbAccessory accessory = UsbManager.getAccessory(intent);
+					UsbAccessory accessory = (UsbAccessory) intent
+							.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+
 					if (intent.getBooleanExtra(
 							UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
 						openAccessory(accessory);
@@ -59,24 +58,25 @@ public class ArduinoActivity extends Activity implements Runnable {
 					mPermissionRequestPending = false;
 				}
 			} else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
-//				UsbAccessory accessory = UsbManager.getAccessory(intent);
-				UsbAccessory accessory = (UsbAccessory) intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+				// UsbAccessory accessory = UsbManager.getAccessory(intent);
+				UsbAccessory accessory = (UsbAccessory) intent
+						.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
 				if (accessory != null && accessory.equals(mAccessory)) {
 					closeAccessory();
 				}
 			}
-			
+
 		}
 	};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.layout_arduino);
 
 		mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-		mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
-				ACTION_USB_PERMISSION), 0);
+		mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+		
 		IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
 		filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
 		registerReceiver(mUsbReceiver, filter);
@@ -86,27 +86,27 @@ public class ArduinoActivity extends Activity implements Runnable {
 			openAccessory(mAccessory);
 		}
 
-//		CheckBox ledButton = (CheckBox) findViewById(R.id.ledCheckBox);
-//		ledButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-//			@Override
-//			public void onCheckedChanged(CompoundButton buttonView,
-//					boolean isChecked) {
-//				if (mOutputStream == null) {
-//					return;
-//				}
-//				// Send command to accessory
-//				byte[] buffer = new byte[2];
-//				buffer[0] = COMMAND_LED;
-//				buffer[1] = isChecked ? (byte) 1 : (byte) 0;
-//
-//				try {
-//					mOutputStream.write(buffer);
-//				} catch (IOException e) {
-//					Log.e(TAG, "write failed", e);
-//				}
-//
-//			}
-//		});
+		// CheckBox ledButton = (CheckBox) findViewById(R.id.ledCheckBox);
+		// ledButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+		// @Override
+		// public void onCheckedChanged(CompoundButton buttonView,
+		// boolean isChecked) {
+		// if (mOutputStream == null) {
+		// return;
+		// }
+		// // Send command to accessory
+		// byte[] buffer = new byte[2];
+		// buffer[0] = COMMAND_LED;
+		// buffer[1] = isChecked ? (byte) 1 : (byte) 0;
+		//
+		// try {
+		// mOutputStream.write(buffer);
+		// } catch (IOException e) {
+		// Log.e(TAG, "write failed", e);
+		// }
+		//
+		// }
+		// });
 	}
 
 	@Override
@@ -173,9 +173,6 @@ public class ArduinoActivity extends Activity implements Runnable {
 			Thread listenThread = new Thread(null, this, "accessoryListener");
 			listenThread.start();
 			Log.d(TAG, "accessory opened");
-
-			final TextView accessoryState = (TextView) findViewById(R.id.tv);
-			accessoryState.setText("Accessory attached");
 		} else {
 			Log.d(TAG, "accessory open fail");
 		}
@@ -190,19 +187,18 @@ public class ArduinoActivity extends Activity implements Runnable {
 		} finally {
 			mFileDescriptor = null;
 			mAccessory = null;
-			final TextView accessoryState = (TextView) findViewById(R.id.tv);
-			accessoryState.setText("Accessory is not attached");
 			finish();
 		}
 	}
 
+	private int count = 0;
+	
 	/**
 	 * Listen accessory's voice
 	 */
 	@Override
 	public void run() {
-		
-		
+
 		int ret = 0;
 		byte[] buffer = new byte[16384];
 		int i;
@@ -217,15 +213,23 @@ public class ArduinoActivity extends Activity implements Runnable {
 			i = 0;
 			while (i < ret) {
 				int len = ret - i;
-
+				
 				switch (buffer[i]) {
-				case 0x1:
-					if (len >= 2) {
-						Message m = Message.obtain(mHandler, MESSAGE_ECHO);
-						m.obj = Integer.valueOf(buffer[i + 1]);
-						mHandler.sendMessage(m);
-					}
-					i += 2;
+//				case 0x1:
+//					if (len >= 2) {
+//						Message m = Message.obtain(mHandler, MESSAGE_ECHO);
+//						m.obj = Integer.valueOf(buffer[i + 1]);
+//						mHandler.sendMessage(m);
+//					}
+//					i += 2;
+//					break;
+//					
+				default:
+					Message m = Message.obtain(mHandler, MESSAGE_ECHO);
+//					m.obj = Integer.valueOf(buffer[i]);
+					m.obj = "ret "+ret +"buffer "+buffer[0]+"count "+count;
+					count++;
+					mHandler.sendMessage(m);
 					break;
 				}
 			}
@@ -237,14 +241,19 @@ public class ArduinoActivity extends Activity implements Runnable {
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
 			case MESSAGE_ECHO:
-				TextView buttonState = (TextView) findViewById(R.id.tv);
-				if ((Integer) msg.obj > 0) {
-					buttonState.setText("Echo : ON");
-				} else {
-					buttonState.setText("Echo : OFF");
-				}
+				TextView buttonState = (TextView) findViewById(R.id.tv1);
+				buttonState.setText((String)msg.obj);
+//				if ((Integer) msg.obj > 0) {
+//					buttonState.setText("Echo : ON");
+//				} else {
+//					buttonState.setText("Echo : OFF");
+//				}
 				break;
 			}
 		}
 	};
+	
+	private void showToast(String str) {
+		Toast.makeText(ArduinoActivity.this, str, Toast.LENGTH_SHORT).show();
+	}
 }
