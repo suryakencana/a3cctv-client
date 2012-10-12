f0733eed5ea678c0a3a3e0c8e11e7034279e5865
daclouds <daclouds@860ef7eb-1e7e-7184-4441-25ebcaecbddb>
2012-10-12 09:28:22 +0000

    카메라 프리뷰 전체화면

--- src/kr/a3cctv/client/camera/StorageServiceImpl.java
+++ src/kr/a3cctv/client/camera/StorageServiceImpl.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.MultipartEntity;
