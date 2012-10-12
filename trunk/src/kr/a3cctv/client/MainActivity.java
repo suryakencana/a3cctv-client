dccb35d0c7b467edc795b5e5eab1dca7f1ae9643
daclouds <daclouds@860ef7eb-1e7e-7184-4441-25ebcaecbddb>
2012-10-12 09:28:22 +0000

    카메라 프리뷰 전체화면

--- trunk/src/kr/a3cctv/client/MainActivity.java
+++ trunk/src/kr/a3cctv/client/MainActivity.java
@@ -5,42 +5,38 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.widget.LinearLayout;
 
 import com.google.android.gcm.GCMRegistrar;
 
 public class MainActivity extends Activity {
-	
+
 	public static final String TAG = "hackfair";
 	public static final String SENDER_ID = "1000906162243";
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_camera);
-        
-        LinearLayout previewContainer = (LinearLayout) findViewById(R.id.previewContainer);
-        Preview preview = new Preview(this);
-        previewContainer.addView(preview);          
-        gcmRegister();
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Preview preview = new Preview(this);
+		setContentView(preview);
+		gcmRegister();
+	}
 
-    private void gcmRegister(){
-    	//에뮬레이터 등에서는 이용 불가. 체크
-        GCMRegistrar.checkDevice(this);
-        //permission 등.
-        GCMRegistrar.checkManifest(this);
-        final String regId = GCMRegistrar.getRegistrationId(this);
-        if (regId.equals("")) {
-        	GCMRegistrar.register(this, SENDER_ID);
-        } else {
-          Log.v(TAG, "Already registered");
-        }
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	private void gcmRegister() {
+		// 에뮬레이터 등에서는 이용 불가. 체크
+		GCMRegistrar.checkDevice(this);
+		// permission 등.
+		GCMRegistrar.checkManifest(this);
+		final String regId = GCMRegistrar.getRegistrationId(this);
+		if (regId.equals("")) {
+			GCMRegistrar.register(this, SENDER_ID);
+		} else {
+			Log.v(TAG, "Already registered");
+		}
+	}
 }
