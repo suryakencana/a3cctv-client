0cfdb49e2d1a6ef6dfe6dd5779b335843eb9e771
ryukw82 <ryukw82@860ef7eb-1e7e-7184-4441-25ebcaecbddb>
2012-10-13 07:42:00 +0000

    업데이트

--- src/kr/a3cctv/client/DialogActivity.java
+++ src/kr/a3cctv/client/DialogActivity.java
@@ -1,20 +1,66 @@
 package kr.a3cctv.client;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
 
 public class DialogActivity extends Activity {
+
+	private static final int REFRESH_INTERVAL = 3000;
+
+	private boolean isAlreadyMove = false;
+
+	Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_gcm);
+		
+		Button btn = (Button) findViewById(R.id.btn);
+
+		if (Util.isGoogleTV(DialogActivity.this)) {
+			btn.setVisibility(View.GONE);
+		} else {
+
+			btn.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					moveWebview();
+
+				}
+			});
+
+		}
+
+		timer.sendEmptyMessageDelayed(0, REFRESH_INTERVAL);
+
 	}
 
-	
-	
+	private Handler timer = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+
+			moveWebview();
+
+		}
+	};
+
+	private void moveWebview() {
+		if (!isAlreadyMove) {
+			Intent i = new Intent(DialogActivity.this, WebViewActivity.class);
+			startActivity(i);
+			isAlreadyMove = true;
+			finish();
+		}
+	}
+
 }
