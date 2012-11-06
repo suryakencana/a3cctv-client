da5b30b2f998c40001cfe67f1f4c85b122244d72
nolleh7707@gmail.com <nolleh7707@gmail.com@860ef7eb-1e7e-7184-4441-25ebcaecbddb>
2012-11-06 02:11:48 +0000

    토큰없을때 / login page 전환 서버로부터 get parameter_

--- trunk/src/kr/a3cctv/client/MainActivity.java
+++ trunk/src/kr/a3cctv/client/MainActivity.java
@@ -2,22 +2,43 @@ package kr.a3cctv.client;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-
-import com.google.android.gcm.GCMRegistrar;
 
 public class MainActivity extends Activity {
 
 	public static final String TAG = "hackfair";
-	public static final String SENDER_ID = "1000906162243";
-
+	
+	private static final int REQ_LOGIN = 0;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		gcmRegister();
+		String token = Util.getToken(getApplicationContext());
+		if(token==null){
+			Util.showToast(getApplicationContext(), "유저 data 없습니다 회원가입/로그인을 위한 전환."); 
+			startActivityForResult(new Intent(this, LoginActivity.class), REQ_LOGIN );
+		}
+		else{
+			Util.showToast(getApplicationContext(), "유저 data 발견. 다음으로 넘어갑니다."); 
+			startNextActivity();
+		}
+		
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		switch(requestCode){
+		case REQ_LOGIN :
+			break;
+		}
+	}
+
+	private void startNextActivity(){
 		
 		if (Util.isGoogleTV(this) && !Util.hasCamera(this)) {
 			//Only viewer
@@ -31,26 +52,12 @@ public class MainActivity extends Activity {
 		}
 		
 		finish();
-		
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
 
-	private void gcmRegister() {
-		// 에뮬레이터 등에서는 이용 불가. 체크
-		GCMRegistrar.checkDevice(this);
-		// permission 등.
-		GCMRegistrar.checkManifest(this);
-		final String regId = GCMRegistrar.getRegistrationId(this);
-		if (regId.equals("")) {
-			GCMRegistrar.register(this, SENDER_ID);
-		} else {
-			Log.v(TAG, "Already registered");
-			Log.d("Ryukw82",GCMRegistrar.getRegistrationId(this));
-		}
-	}
 }
--- trunk/src/kr/a3cctv/client/Util.java
+++ trunk/src/kr/a3cctv/client/Util.java
@@ -1,9 +1,15 @@
 package kr.a3cctv.client;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageManager;
+import android.widget.Toast;
 
 public class Util {
+	
+	public static final String SHARED_DATA = "a3shared";
+	public static final String KEY_TOKEN = "token";
 	
 	public static boolean isGoogleTV(Context context) {
     	return context.getPackageManager().hasSystemFeature("com.google.android.tv");
@@ -12,5 +18,19 @@ public class Util {
 	public static boolean hasCamera(Context context) {
 		return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
+	public static void showToast(Context context, String msg){
+		Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+	}
 
+	public static String getToken(Context context){
+		return loadPref(context).getString(KEY_TOKEN, null);
+	}
+	
+	public static void setToken(Context context, String token){
+		Editor pref = loadPref(context).edit(); 
+		pref.putString(KEY_TOKEN, token);
+	}
+	public static SharedPreferences loadPref(Context context){
+		return context.getSharedPreferences(SHARED_DATA, Context.MODE_PRIVATE);
+	}
 }
